364eb05ac0cfdc32c05a2b6be972b4abbc4c896b
Weston Silbaugh <7364022+westnt@users.noreply.github.com>
2018-07-17 14:01:32 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+
+    public static void Main()
+    {
+        Console.WriteLine("test");
+    }
+}
+
+
+[System.ComponentModel.RunInstaller(true)]
+public class Sample : System.Configuration.Install.Installer
+{
+    public override void Uninstall(System.Collections.IDictionary savedState)
+    {
+        Console.WriteLine("pow");
+    }
+}
